733318d237bf8b0b49586be3713ab14b7515d338
Sheikh Haris Zahid <sheikh.haris.zahid@hotmail.com>
2020-06-10 17:03:08 +0500

    initial commit-1

--- InvApp/settings.py
+++ InvApp/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
+import django_heroku
 from django.contrib.messages import constants as messages
 import os
 
@@ -154,3 +155,5 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
+# Activate django_heroku
+django_heroku.settings(locals())
